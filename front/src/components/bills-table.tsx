355e38bdf5e311b881482077ae6a5c622d897076
jlwetler <joaolucasmw@gmail.com>
2025-01-25 11:33:31 -0300

    feat: add table filter

--- front/src/components/bills-table.tsx
+++ front/src/components/bills-table.tsx
@@ -5,23 +5,25 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import { FaArrowDown, FaSearchengin } from "react-icons/fa6";
+import { useState } from "react";
+import { FaSearchengin } from "react-icons/fa6";
 
 const columns: ColumnDef<IBill>[] = [
   {
     accessorKey: "id",
     header: "ID",
     cell: ({ row }) => {
-      return <div className="m-1">{row.original.id}</div>;
+      return <div className="m-1 w-20">{row.original.id}</div>;
     },
   },
   {
     accessorKey: "title",
     header: "Bill",
     cell: ({ row }) => {
-      return <div className="m-1">{row.original.title}</div>;
+      return <div className="m-1 w-96">{row.original.title}</div>;
     },
   },
   {
@@ -53,29 +55,34 @@ const columns: ColumnDef<IBill>[] = [
 
 export default function BillsTable() {
   const { data: bills, isLoading, error } = useGetBills();
+  const [globalFilter, setGlobalFilter] = useState("");
 
   const table = useReactTable<IBill>({
     columns,
     data: bills ?? [],
+    state: { globalFilter },
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
   });
 
   if (isLoading && !bills) return <div>Loading...</div>;
   if (error && !bills) return <div>Error: {error.message}</div>;
 
-  console.log(bills);
   return (
     <div>
-      <div className="mb-2 flex items-center relative">
+      <div className="font-bold text-4xl">Bills</div>
+      <div className="my-3 flex items-center relative">
         <FaSearchengin size={16} className="absolute left-2" />
         <input
-          className="input input-info input-sm pl-8"
+          className="w-72 pl-10 py-1 rounded-lg bg-gray-700 text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="Buscar"
+          value={globalFilter}
+          onChange={(e) => setGlobalFilter(e.target.value)}
         />
       </div>
       <table className="table">
         <thead>
-          <tr>
+          <tr className="rounded-t-lg border-2">
             {table.getHeaderGroups()[0].headers.map((header) => (
               <th key={header.id}>
                 <div className="flex gap-2 m-1">
@@ -83,22 +90,32 @@ export default function BillsTable() {
                     header.column.columnDef.header,
                     header.getContext()
                   )}
-                  <FaArrowDown size={16} className="cursor-pointer my-auto" />
                 </div>
               </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <tr key={row.id} className="rounded-lg border-2">
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td
+                colSpan={columns.length}
+                className="text-center mx-auto w-[730px] py-4"
+              >
+                Nenhum item encontrado
+              </td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
     </div>
--- front/src/components/legislators-table.tsx
+++ front/src/components/legislators-table.tsx
@@ -5,23 +5,25 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import { FaArrowDown, FaSearchengin } from "react-icons/fa6";
+import { useState } from "react";
+import { FaSearchengin } from "react-icons/fa6";
 
 const columns: ColumnDef<ILegislator>[] = [
   {
     accessorKey: "id",
     header: "ID",
     cell: ({ row }) => {
-      return <div className="m-1">{row.original.id}</div>;
+      return <div className="m-1 w-20">{row.original.id}</div>;
     },
   },
   {
     accessorKey: "name",
     header: "Legislator",
     cell: ({ row }) => {
-      return <div className=" m-1">{row.original.name}</div>;
+      return <div className="m-1 w-96">{row.original.name}</div>;
     },
   },
   {
@@ -50,50 +52,66 @@ const columns: ColumnDef<ILegislator>[] = [
 
 export default function LegislatorsTable() {
   const { data: legislators, isLoading, error } = useGetLegislators();
+  const [globalFilter, setGlobalFilter] = useState("");
 
   const table = useReactTable<ILegislator>({
     columns,
     data: legislators ?? [],
+    state: { globalFilter },
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
   });
   if (isLoading && !legislators) return <div>Loading...</div>;
   if (error && !legislators) return <div>Error: {error.message}</div>;
 
   return (
     <div>
-      <div className="mb-2 flex items-center relative">
+      <div className="font-bold text-4xl">Legislators</div>
+      <div className="my-3 flex items-center relative">
         <FaSearchengin size={16} className="absolute left-2" />
         <input
-          className="input input-info input-sm pl-8"
+          className="w-72 pl-10 py-1 rounded-lg bg-gray-700 text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="Buscar"
+          value={globalFilter}
+          onChange={(e) => setGlobalFilter(e.target.value)}
         />
       </div>
       <table className="table">
         <thead>
-          <tr>
+          <tr className="rounded-lg border-2">
             {table.getHeaderGroups()[0].headers.map((header) => (
-              <th key={header.id}>
+              <th key={header.id} className="">
                 <div className="flex gap-2 m-1 align-center">
                   {flexRender(
                     header.column.columnDef.header,
                     header.getContext()
                   )}
-                  <FaArrowDown size={16} className="cursor-pointer my-auto" />
                 </div>
               </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <tr key={row.id} className="rounded-lg border-2">
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td
+                colSpan={columns.length}
+                className="text-center mx-auto w-[730px] py-4"
+              >
+                Nenhum item encontrado
+              </td>
             </tr>
-          ))}
+          )}
         </tbody>
       </table>
     </div>
